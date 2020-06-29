98a568c8d8da6db6d1b12401b2106cfde6379f4d
Anish <anish.singhani@gmail.com>
2020-06-29 04:15:48 +0000

    Remove test code from top module

--- top.sv
+++ top.sv
@@ -2,34 +2,11 @@
 `default_nettype none
 `endif
 
-`include "decode/instruction.sv"
-
 module top (
     output wire [7:0] led,
     input wire [4:0] btn,
 
     input wire i_clk
 );
-
-wire bonk;
-
-btn_stb btn_stb (
-    .o_press_stb(bonk),
-    .i_clk(i_clk),
-    .i_btn(btn[4])
-);
-
-instruction_t inst;
-instruction_t inst2;
-
-decode decode (
-    .i_instr({20'b0, btn[3:0], 8'b0}),
-    .o_out(inst),
-    .o_out2(inst2),
-    .bonk(bonk),
-    .i_clk(i_clk)
-);
-
-assign led = {inst2.rd_addr[3:0], inst.rd_addr[3:0]};
 
 endmodule
